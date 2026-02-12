7d2d44abd1192f465134d86bdc5701da379e8018
vladyslavyatsuta <vy2000eng@gmail.com>
2026-02-11 21:33:40 -0500

    yea ofc we dont need the user entity

--- drf.Domain/Entities/User.cs
+++ drf.Domain/Entities/User.cs
@@ -8,13 +8,13 @@ public class User:IdentityUser<Guid>
     public string? RefreshToken { get; set; }
     public DateTime RefreshTokenExpiry { get; set; }
     public DateTime CreatedOn { get; set; }
-    public Boolean IsSubscribed { get; set; }
+    //public Boolean IsSubscribed { get; set; }
     public string? GoogleId { get; set; } // Store the subject here
     public string AuthProvider { get; set; } = "Local";
     public string? EmailVerificationCode { get; set; }
     public DateTime? EmailVerificationCodeExpiry { get; set; }
-    public DateTime? TimeSinceLastAnalysis { get; set; }
-    public DateTime? TimeSinceLastRecording { get; set; }
+    //public DateTime? TimeSinceLastAnalysis { get; set; }
+    //public DateTime? TimeSinceLastRecording { get; set; }
 
 
     public List<Dream>? Dreams { get; set; }
@@ -32,10 +32,10 @@ public class User:IdentityUser<Guid>
             Email        = email,
             UserName     = email,
             CreatedOn    = DateTime.UtcNow,
-            IsSubscribed = false,
+          //  IsSubscribed = false,
             AuthProvider = authProvider, // Add this
-            TimeSinceLastAnalysis = null,
-            TimeSinceLastRecording = null
+            //TimeSinceLastAnalysis = null,
+            //TimeSinceLastRecording = null
             
             
         };
